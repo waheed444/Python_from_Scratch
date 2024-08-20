5992f973f45d4799ed3080190811f4da49fef317
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-08-20 20:56:08 +0500

    Add files via upload

---
+++ 01_Intro_to_Python/1_Hello_World.py
@@ -0,0 +1,15 @@
+#print is used to display the data on the screen
+#() is called paranthesis 
+#The data is printed on the screen as it is in the double quotes ""
+
+print("HELLO WORLD")    
+
+# its is also used to calculation purposed
+
+print(5*5)
+
+#we can print multiple statement in one line separated by commas ","
+
+print("Hello WAHEED",5*5,"This is print statement")
+
+#python is interpretted language and run the code line by line not compiler based languge
