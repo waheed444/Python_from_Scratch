99945a5b44a15f136b2b19ed9f62a3caaf729772
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-08-25 09:10:28 +0500

    Variable & Strings

---
+++ 01_Intro_to_Python/2_Variable_Strings.py
@@ -0,0 +1,17 @@
+#varaibles are the memory location (containers)  that is used to store the data
+# "="  is called  assigment opeartor that used to assign the value.
+
+name = "Waheed"               #Waheed is string 
+age = 21                      #age is  variable
+gpa =3.34                     #gpa is  variable
+ 
+    
+# Print the data that stored in the variables
+
+print("My name is  :",name)
+print("My age is   :",age)
+print("My GPA is   :",gpa)
+
+# There are many rule of nameing the variable
+
+
