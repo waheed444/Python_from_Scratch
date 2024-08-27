99eaf84985c48c0c26f6650637c58f0160f26ccf
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-08-27 21:10:04 +0500

    Keywords in Python

---
+++ 01_Intro_to_Python/3_Keywords.py
@@ -0,0 +1,16 @@
+#Keywords are the reserve words in pytyhon and other programming language.
+#They have predefined meaning in the python.
+#There are many keywords are used in python.
+# Keyword name can't be used as varaiable name
+# #print
+# input
+# if
+#while
+#int
+# else
+# True
+# False
+
+
+print("HELLO WORLD") 
+# print is the keyword in python and it meaning already predefined in the python language to print a statment
